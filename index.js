4c004cded04e1e8573042d09b6d2bd38199ea239
NunezAN <alex101nunez@gmail.com>
2022-07-11 13:38:00 -0500

    added moving background

--- index.js
+++ index.js
@@ -4,6 +4,19 @@
 
 let isModalOpen =false;
 let contrastToggle = true;
+const scaleFactor = 1/20;
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    for(i=0;i<shapes.length;i++)
+    {
+        const isOdd = i % 2 !== 0;
+        const booleanInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * booleanInt}px,${y * booleanInt}px)`;
+    }
+}
 
 function toggleContrast(){
     contrastToggle = !contrastToggle;
